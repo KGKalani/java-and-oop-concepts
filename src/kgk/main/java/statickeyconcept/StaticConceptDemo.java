f144d5e0d5b060893dff6308397afcce65c1373b
Kalani <kalani.gayathri91@gmail.com>
2022-05-28 12:27:37 -0700

    Java static concept

---
+++ src/kgk/main/java/statickeyconcept/StaticConceptDemo.java
@@ -0,0 +1,54 @@
+package kgk.main.java.statickeyconcept;
+
+/**
+ * static keyword can be applied with
+ *  > variables (Class variable)
+ *  > methods (Class method)
+ *  > blocks
+ *  > nested classes
+ */
+public class StaticConceptDemo {
+    int studentID;
+    String studentName;
+    String studentDepartment;
+
+    /*static variable (Class variable*/
+    static String uniName;
+
+    /*
+      static block
+        > This is used to initialize the static data member.
+        > This is executed at the time of classloading. (before all methods in the class is called)
+     */
+    static {
+        System.out.println(" Static Block");
+        uniName = "UOM";
+    }
+
+    public StaticConceptDemo(int id, String name, String dept){
+        studentID = id;
+        studentName = name;
+        studentDepartment = dept;
+    }
+    public void displayDetails(){
+        System.out.println("id : "+ studentID);
+        System.out.println("name : "+ studentName);
+        System.out.println("dept : "+ studentDepartment);
+        System.out.println("university : "+ uniName);
+    }
+
+    /*
+     * static method
+     * non-static variables cannot be accessed in static methods
+     */
+    /* public static void displayDetails(){
+        System.out.println("id : "+ studentID);
+        System.out.println("name : "+ studentName);
+        System.out.println("dept : "+ studentDepartment);
+        System.out.println("university : "+ uniName);
+    }*/
+}
+
+
+
+
