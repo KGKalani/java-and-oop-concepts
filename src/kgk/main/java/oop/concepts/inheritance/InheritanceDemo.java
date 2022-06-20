553630c628419283da096e43b6a462769eaa1a5a
Kalani <kalani.gayathri91@gmail.com>
2022-06-20 07:42:42 -0700

    Implement instanceof operator demo

---
+++ src/kgk/main/java/oop/concepts/inheritance/InheritanceDemo.java
@@ -0,0 +1,9 @@
+package kgk.main.java.oop.concepts.inheritance;
+
+public class InheritanceDemo {
+    public static void demo() {
+        Dog d = new Dog();
+        d.bark();
+        d.eat();
+    }
+}
