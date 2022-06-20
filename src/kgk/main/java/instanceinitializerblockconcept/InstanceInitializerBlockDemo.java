82e8f98db72e5d79552d70d40d76d03808cc7b1f
Kalani <kalani.gayathri91@gmail.com>
2022-06-20 08:04:14 -0700

    Implement instance initializer block demo

---
+++ src/kgk/main/java/instanceinitializerblockconcept/InstanceInitializerBlockDemo.java
@@ -0,0 +1,8 @@
+package kgk.main.java.instanceinitializerblockconcept;
+
+public class InstanceInitializerBlockDemo {
+    public static void main(String args[]) {
+        InstanceInitializerBlock instanceInitializerBlockOne = new InstanceInitializerBlock();
+        InstanceInitializerBlock instanceInitializerBlockOne2 = new InstanceInitializerBlock();
+    }
+}
---
+++ src/kgk/main/java/instanceinitializerblockconcept/InstanceInitializerBlockParent.java
@@ -0,0 +1,9 @@
+package kgk.main.java.instanceinitializerblockconcept;
+
+public class InstanceInitializerBlockParent {
+    public InstanceInitializerBlockParent(){
+        System.out.println("Parent class is invoked");
+    }
+
+
+}
